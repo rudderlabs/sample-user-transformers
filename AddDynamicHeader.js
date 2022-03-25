ba7cb1a5d61680af938ece5f7d4fad5dc452cfb2
Emil Sadek <esadek@hotmail.com>
2022-03-25 15:16:58 -0700

    Remove: transformation files

--- AddDynamicHeader.js
+++
@@ -1,9 +0,0 @@
-// The following code adds a dynamic header for your events through user transformation, for the Webhooks destination.
-
-export function transformEvent(event, metadata) {
-  event.header = {
-    dynamic_header_1: "dynamic_header_value"
-  };
-
-  return event;
-}
